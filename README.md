7d9eb28a9f16ff5143d062107be6dd37accaf76a
Marcus A. Sales Jr <delphi.marcus@gmail.com>
2017-07-13 22:38:27 -0300

    ajuste

--- README.md
+++ README.md
@@ -3,14 +3,6 @@
 # Implementação do Game - Torres
 # Aluno : Marcus Antonio Sales Junior
 # Matricula : 201165251AC
-
-Controle
-PLAYER 1 :
-  ^
-  |
-<- ->
-  |
-  v
 ```
 <p align="center">
   <img src="tela.png" width="100%"/>
--- js/Level.js
+++ js/Level.js
@@ -3,20 +3,12 @@
  */
 function Level() {
     this.grid = null;
-    this.player_1 = null;
-    this.player_2 = null;
 }
 
-Level.prototype.init = function (grid, player_1, player_2) {
+Level.prototype.init = function (grid) {
     this.grid = grid;
-    this.player_1 = player_1;
-    this.player_2 = player_2;
 }
 
-Level.prototype.desenha = function (contexto, dt) {
+Level.prototype.desenha = function (contexto) {
     this.grid.desenha(contexto);
-    this.player_1.mover(dt);
-    this.player_1.desenha(contexto);
-    this.player_2.mover(dt);
-    this.player_2.desenha(contexto);
 }
